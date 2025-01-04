bbc67c30036335d2f16db8b246a3132cc18e17a0
talhahzahid <talhahzahid415@gmail.com>
2025-01-04 08:12:19 +0500

    Client

--- Client/src/Pages/SignIn.jsx
+++ Client/src/Pages/SignIn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SignIn = () => {
   return (
     <>
-      <h1>Sign in</h1>
+      <h1>Sign in</h1>  
     </>
   );
 };
--- Client/src/Pages/SignUp.jsx
+++ Client/src/Pages/SignUp.jsx
@@ -4,28 +4,68 @@ const SignUp = () => {
   const fullname = useRef();
   const email = useRef();
   const password = useRef();
-  const image = useRef();
+  const imageUrl = useRef();
   const [loading, setLoading] = useState(false);
-  const signUphandle = (event) => {
+  const [error, setError] = useState("");
+
+  const signUphandle = async (event) => {
     event.preventDefault();
-    console.log(fullname.current.value);
-    console.log(email.current.value);
-    console.log(password.current.value);
-    console.log(image.current.value);
-    const data = fetch('')
+    const fullNameValue = fullname.current.value;
+    const emailValue = email.current.value;
+    const passwordValue = password.current.value;
+    const file = imageUrl.current.files[0];
+    setLoading(true);
+    // Create a FormData object and append the data
+    const formData = new FormData();
+    formData.append("fullname", fullNameValue);
+    formData.append("email", emailValue);
+    formData.append("password", passwordValue);
+    formData.append("imageUrl", file);
+
+    try {
+      const response = await fetch("http://localhost:9000/user/signup", {
+        method: "POST",
+        body: formData,
+        credentials: "include",
+      });
+
+      // const data = await response.text(); // Use .text() to log the raw response text
+      if (response.ok) {
+        console.log("User registered successfully:", data);
+      } else {
+        console.error("Error:", response.error.data.message, data);
+      }
+    } catch (error) {
+      setError(response.error.data.message);
+      console.error("Error during signup:", error.message);
+      // Handle network error or unexpected error
+    } finally {
+      setLoading(false); // Stop loading
+    }
+
+    fullname.current.value = "";
+    email.current.value = "";
+    password.current.value = "";
+    imageUrl.current.value = "";
   };
+
+  // {error && (
+  //   <div className="text-red-500 text-sm mb-2">
+  //     <p>{error}</p>
+  //   </div>
+
   return (
     <>
       <div className="flex justify-center items-center h-[100vh] px-7">
         <form
           onSubmit={signUphandle}
-          action=""
           className="flex flex-col flex-wrap justify-center items-center gap-4 bg-[#f6f7f9] rounded-lg shadow-lg p-10"
         >
           <h1 className="text-2xl font-bold text-[#ff6600]"> Sign Up</h1>
           <input
             type="text"
             placeholder="Enter FullName"
+            name="fullname"
             required
             className="input input-bordered w-full max-w-xs"
             ref={fullname}
@@ -33,6 +73,7 @@ const SignUp = () => {
           <input
             type="email"
             placeholder="Enter Email"
+            name="email"
             required
             className="input input-bordered w-full max-w-xs"
             ref={email}
@@ -40,6 +81,7 @@ const SignUp = () => {
           <input
             type="password"
             placeholder="Enter password"
+            name="password"
             required
             className="input input-bordered w-full max-w-xs"
             ref={password}
@@ -48,7 +90,7 @@ const SignUp = () => {
             type="file"
             className="file-input file-input-bordered w-full max-w-xs"
             required
-            ref={image}
+            ref={imageUrl}
           />
           <button className="btn bg-[#ff6600] text-white" type="submit">
             {loading ? (
